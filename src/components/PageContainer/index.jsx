365a8f48b2aa37e49c2fab63b4fc7add31603851
민우석 <s23044@gsm.hs.kr>
2025-07-14 10:23:28 +0900

    fix button

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -218,7 +218,7 @@ function PageContainer({
             value={comment}
             onChange={e => setComment(e.target.value)}
           />
-          <S.SubmitButton type="submit">
+          <S.SubmitButton type="submit" onClick={() => fetch()}>
             댓글 달기
           </S.SubmitButton>
         </S.InputBox>
